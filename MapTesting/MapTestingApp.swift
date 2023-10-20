c336877731a8cd6cde53aad73d788c5787bbbf1e
ZethT <zethtang@gmail.com>
2023-10-20 00:12:06 -0700

    template copied

---
+++ MapTesting/MapTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestingApp.swift
+//  MapTesting
+//
+//  Created by Zeth Tang on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
